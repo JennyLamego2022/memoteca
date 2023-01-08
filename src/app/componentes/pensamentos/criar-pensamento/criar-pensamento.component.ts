758bf22c18ba145ccb42c39b9a02bf8bab1841b2
JennyLamego2022 <jennylamego@hotmail.com>
2023-01-08 20:45:58 -0300

    feat: criaçao do card de pensamento

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class CriarPensamentoComponent {
 
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: '',
+  }
+
 }
